49a80399cf6ca65b3b2b7bfa5a52ed81ab6d09b4
Bogdan <email@server.domen>
2022-06-18 15:46:13 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WavePageControl
+
+A description of this package.
---
+++ Sources/WavePageControl/WavePageControl.swift
@@ -0,0 +1,6 @@
+public struct WavePageControl {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
